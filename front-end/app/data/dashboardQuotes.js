8d294efa75b815b8515f4e11f6273c96fe33e1a5
Jordan Rios <jordanriosorjuela@gmail.com>
2025-01-27 23:47:22 +0000

    added randomised quote in the dashboard

---
+++ front-end/app/data/dashboardQuotes.js
@@ -0,0 +1,7 @@
+export const quotes = [
+    "Let Your Voice Do the Typing...",
+    "Say it, we'll write it...",
+    "Transcribing made easy...",
+    "Let the technology work for you...",
+    "Your voice matters..."
+];
--- front-end/app/speech-dash-board/page.tsx
+++ front-end/app/speech-dash-board/page.tsx
@@ -1,28 +1,53 @@
 "use client";
-import React, {useState} from "react";
-import { useRouter } from "next/navigation";
-import Link from 'next/link'
+import React, { useEffect, useState } from "react";
+import { quotes } from "../data/dashboardQuotes";
 
-export default function speechDashboard () {
+export default function speechDashboard() {
+  const [quote, setRandomQuote] = useState("");
 
-return(
-<div className="bg-gray-200 font-sans text-gray-700">
-  <div className="container mx-auto p-8 flex">
-    <div className="max-w-md w-full mx-auto">
-      <h1 className="text-4xl text-center mb-12 font-thin">Speech to Text</h1>
-      <main className="flex flex-col items-center text-center flex-grow">
-        <textarea
-          className="mt-2 ml-12 w-[500px] h-[200px] bg-black/5 border-b border-l border-solid border-white shadow-inner rounded-lg p-2 focus:outline-none focus:bg-white transition-colors ease-in-out"
-        ></textarea>
-        <button className="w-full text-2xl p-3 mt-4 bg-blue-600 text-white rounded shadow">
-          Play
-        </button>
-      </main>
-      <footer id="footer" className="text-center bg-gray-800 text-white fixed bottom-0 w-full py-4">
-        Footer Content
-      </footer>
+  useEffect(() => {
+    setRandomQuote(quotes[Math.floor(Math.random() * quotes.length)]);
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-gray-200">
+      <div className="bg-gray-200 font-sans text-gray-700">
+        <div className="container mx-auto p-8">
+          <div className="max-w-md w-full mx-auto">
+            <h1 className="text-7xl text-center mb-12 font-thin">
+              Speech to Text
+            </h1>
+            <div className="flex justify-center">
+              <main className="flex flex-col items-center text-center">
+                <h2 className="text-3xl texxt-center mb-12 font-bold">
+                  "{quote}"
+                </h2>
+                <textarea
+                  className="mt-4 w-[600px] h-[300px] bg-black/5 border-b border-l border-solid border-white shadow-inner rounded-lg p-4 focus:outline-none focus:bg-white transition-colors ease-in-out"
+                  placeholder="Press 'Play' to start recording..."
+                ></textarea>
+                <div
+                  style={{
+                    display: "grid",
+                    gridTemplateColumns: "auto auto auto",
+                    gap: "20px",
+                  }}
+                >
+                  <button className="w-full text-2xl p-3 mt-4 bg-blue-600 text-white rounded shadow">
+                    Play
+                  </button>
+                  <button className="w-full text-2xl p-3 mt-4 bg-blue-600 text-white rounded shadow">
+                    Pause
+                  </button>
+                  <button className="w-full text-2xl p-3 mt-4 bg-blue-600 text-white rounded shadow">
+                    Stop
+                  </button>
+                </div>
+              </main>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-);
-};
+  );
+}
